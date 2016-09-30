d8e918432cab738fbc8096243606ba5af75c999b
Vinit Kumar <vinitbhu06@gmail.com>
2016-09-30 15:43:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,8 +110,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 //app.use('/ui/libform', express.static(__dirname + '/ui/libform'));
-//app.use(express.static(__dirname + '/ui/libform/'));
-app.use(express.static(path.join(__dirname, 'ui/libform/')));
+app.use(express.static(__dirname + 'ui/libform/'));
+//app.use(express.static(path.join(__dirname, 'ui/libform/')));
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
